a4ffefd1ddac097e792407b2ba3ac56537c12deb
robotkitkat6 <robotkitkat6@gmail.com>
2025-12-04 23:20:20 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,15 @@ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12
 
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+  apiKey: "AIzaSyCUQpfPPlJqLRm9FyryfX2UOa82mUWbJrE",
+  authDomain: "robotkitkat6-bee.firebaseapp.com",
+  databaseURL: "https://robotkitkat6-bee-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "robotkitkat6-bee",
+  storageBucket: "robotkitkat6-bee.firebasestorage.app",
+  messagingSenderId: "117130770804",
+  appId: "1:117130770804:web:4827cc61a7c9df99549421",
+  measurementId: "G-WMKP4GT930"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
